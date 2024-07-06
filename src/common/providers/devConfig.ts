8061c371f09821927a2153ca57c102015c346981
Naveen2070 <104181086+Naveen2070@users.noreply.github.com>
2024-07-06 09:47:03 +0530

    testing remote change

--- src/common/providers/devConfig.ts
+++ src/common/providers/devConfig.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * The Service class is responsible for fetching the dev config
+ * environment variables in the application
+ */
 @Injectable()
 export class DevConfigService {
-  DBHOST = 'localhost';
-  getDBHOST() {
-    return this.DBHOST;
+  /**
+   * The private object holding the dev config env variables
+   */
+  private readonly config: { DBHOST: string };
+
+  /**
+   * The constructor function is responsible for setting the default
+   * value for the DBHOST config env variable
+   */
+  constructor() {
+    this.config = {
+      DBHOST: process.env.DBHOST || 'localhost',
+    };
+  }
+
+  /**
+   * The getDBHOST function is responsible for fetching the DBHOST
+   * config env variable
+   *
+   * @returns {string} DBHOST config env variable
+   */
+  getDBHOST(): string {
+    return this.config.DBHOST;
   }
 }
